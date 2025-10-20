26d656a77b3e182bc8cf16db9d5f21fb121ad7a3
mxl9236 <mxl9236@gmail.com>
2025-10-20 05:04:37 -0700

    lite

--- target/linux/amlogic/image/s905_autoscript.cmd
+++ target/linux/amlogic/image/s905_autoscript.cmd
@@ -1,16 +1,11 @@
-if test "${usbdev}" = ""; then
-	setenv devtype "mmc"
-	setenv devnum 0
-else
-	setenv devtype "usb"
-	setenv devnum ${usbdev}
-fi
-
-if fatload ${devtype} ${devnum} 0x1000000 u-boot.emmc; then go 0x1000000; fi;
-setenv dtb_addr 0x1000000
+if fatload usb 1 0x1000000 u-boot.ext; then go 0x1000000; fi;
+if fatload usb 0 0x1000000 u-boot.ext; then go 0x1000000; fi;
 setenv env_addr 0x1040000
-setenv kernel_addr 0x11000000
-setenv initrd_addr 0x13000000
-setenv boot_start booti ${kernel_addr} ${initrd_addr} ${dtb_addr}
-setenv addmac 'if printenv mac; then setenv bootargs ${bootargs} mac=${mac}; elif printenv eth_mac; then setenv bootargs ${bootargs} mac=${eth_mac}; elif printenv ethaddr; then setenv bootargs ${bootargs} mac=${ethaddr}; fi'
-if fatload ${devtype} ${devnum} ${env_addr} uEnv.txt && env import -t ${env_addr} ${filesize}; setenv bootargs ${APPEND}; then if fatload ${devtype} ${devnum} ${kernel_addr} ${LINUX}; then if fatload ${devtype} ${devnum} ${initrd_addr} ${INITRD}; then if fatload ${devtype} ${devnum} ${dtb_addr} ${FDT}; then run addmac; run boot_start; fi; fi; fi; fi;
+setenv boot_start 'bootm ${loadaddr} - ${dtb_mem_addr}'
+setenv addmac 'if printenv mac; then setenv bootargs ${bootargs} mac=${mac}; elif printenv eth_mac; then setenv bootargs ${bootargs} mac=${eth_mac}; fi'
+setenv try_boot_start 'if fatload ${devtype} ${devnum} ${loadaddr} KERNEL; then fatload ${devtype} ${devnum} ${env_addr} uEnv.ini && env import -t ${env_addr} ${filesize} && run addmac; fatload ${devtype} ${devnum} ${dtb_mem_addr} ${dtb_name} && run boot_start; fi'
+setenv devtype mmc
+setenv devnum 0
+run try_boot_start
+setenv devtype usb
+for devnum in 0 1 2 3 ; do run try_boot_start ; done
